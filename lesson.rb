12423f4520e821ad918c950442b924497c8f4abd
masakichi555 <m.n.m.s.k.0909@gmail.com>
2025-08-10 13:04:58 +0900

    ruby練習　初期コミット

---
+++ lesson.rb
@@ -0,0 +1,14 @@
+#fizzbizz
+def fizzbizz(n)
+  (1..n).each do |i|
+    if i % 15 == 0
+      puts "FizzBizz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Bizz"
+    else
+      puts i
+    end
+  end
+end
